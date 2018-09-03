81d240e1e24d57e614c2246e03108c4a5028eb2c
davidputman <davidputman@davidputman.com>
2018-09-03 11:12:59 +0100

    placeholder test

---
+++ TheProject.Test/Unit/DummyTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TheProject.Test.Unit
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void ThisIsJustAPlaceholder()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
