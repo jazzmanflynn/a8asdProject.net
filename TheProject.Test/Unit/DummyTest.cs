a0903ec64e24d8e93e259b3789df40057d1031a3
david putman <davidputman@davidputman.com>
2018-11-21 23:11:33 +0100

    the sanity clause

---
+++ TheProject.Test/Unit/DummyTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TheProject.Test.Unit
+{
+    public class DummyTest
+    {
+        [Test]
+        public void TheSanityClause()
+        {
+            Assert.IsTrue(true);
+    }
+}
